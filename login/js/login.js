e77c81abdc80407ec64fb988fbee54868df36fff
RdzSebastian <rdzsebastian@gmail.com>
2020-07-28 21:29:58 -0300

    Add login page in webadmin button

---
+++ login/js/login.js
@@ -0,0 +1,21 @@
+// Below function Executes on click of login button.
+function validate(){
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+	
+	var admin = getUrlVars()["admin"];
+	
+	if ( username == "server" && password == "os3os"){
+		window.location = "../admin/" + admin + ".html"; // Redirecting to other page.
+	} else{
+		window.location = "../admin/error.html"; // Redirecting to other page.
+	}
+}
+
+function getUrlVars() {
+	var vars = {}; 
+	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) { 
+	   vars[key] = value; 
+	});
+	return vars; 
+}
